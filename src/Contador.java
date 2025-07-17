8087a660a44abd626f9d672a1b2225b5ee19eaa5
Eduardo <msdohko@gmail.com>
2025-07-17 02:35:43 -0300

    primeiro commit

---
+++ src/Contador.java
@@ -0,0 +1,11 @@
+public class Contador {
+
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 10) {
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
